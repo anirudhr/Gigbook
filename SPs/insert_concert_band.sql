ce347ade1bb8f92ec7267d2df94c8ad63dd89f72
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-15 17:26:12 -0500

    sp

--- SPs/insert_concert_band.sql
+++ SPs/insert_concert_band.sql
@@ -1,2 +1,8 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
-/*SP to insert concert information as a(?) band that is playing the concert*/
+/*SP to insert concert information as a(?) band that is playing in the concert*/
+DELIMITER //
+CREATE PROCEDURE procname()
+BEGIN
+    //
+END//
+DELIMITER ;
