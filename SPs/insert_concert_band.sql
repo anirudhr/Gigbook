c30c76ca4bb362ae2309e33df0750f95360ea66d
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-15 18:04:48 -0500

    More SP work done. Decided to unify band and system insertion/updation SPs.

--- SPs/insert_concert_band.sql
+++
@@ -1,8 +0,0 @@
-/*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
-/*SP to insert concert information as a(?) band that is playing in the concert*/
-DELIMITER //
-CREATE PROCEDURE procname()
-BEGIN
-    //
-END//
-DELIMITER ;
--- SPs/update_concert_system.sql
+++
@@ -1,1 +0,0 @@
-/*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
