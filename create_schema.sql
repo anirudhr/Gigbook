5cd0ceb52bb0be8ad9c57c00bb301d8cb4076ca7
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-16 16:45:21 -0500

    corrected datatype error, increased varchar lengths from 20 to 50

--- create_schema.sql
+++ create_schema.sql
@@ -18,12 +18,12 @@ DROP TABLE IF EXISTS venue;
 DROP TABLE IF EXISTS band;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS genre;
-CREATE TABLE user (     uname VARCHAR(20),
-                        lastname VARCHAR(20) NOT NULL,
-                        firstname VARCHAR(20) NOT NULL,
+CREATE TABLE user (     uname VARCHAR(50),
+                        lastname VARCHAR(50) NOT NULL,
+                        firstname VARCHAR(50) NOT NULL,
                         password VARCHAR(64) NOT NULL,
                         lastlogintime CHAR(19) NULL,  /*19 = sizeof('yyyy-mm-dd hh:mm:ss')*/
-                        city VARCHAR(20) NOT NULL,
+                        city VARCHAR(50) NOT NULL,
                         birthdate CHAR(10) NOT NULL, /*10 = sizeof('yyyy-mm-dd')*/
                         email VARCHAR(50) NOT NULL, /*CONSTRAINT email_format CHECK (REGEXP_LIKE (email, '^\w+(\.\w+)*+@\w+(\.\w+)+$'))*/
                         joindate CHAR(10) NOT NULL,
@@ -31,22 +31,22 @@ CREATE TABLE user (     uname VARCHAR(20),
                         PRIMARY KEY (uname),
                         UNIQUE (email)
                     );
-CREATE TABLE band   (   bname VARCHAR(20),
+CREATE TABLE band   (   bname VARCHAR(50),
                         bio TEXT,
                         PRIMARY KEY (bname)
                     );
 CREATE TABLE venue  (
-                        vname VARCHAR(20),
-                        building VARCHAR(5) NOT NULL, street VARCHAR(20) NOT NULL,
-                        city VARCHAR(20) NOT NULL, state char(2) NOT NULL, zip CHAR(5) NOT NULL,
+                        vname VARCHAR(50),
+                        building VARCHAR(5) NOT NULL, street VARCHAR(50) NOT NULL,
+                        city VARCHAR(50) NOT NULL, state char(2) NOT NULL, zip CHAR(5) NOT NULL,
                         capacity int,
                         url varchar(64) NULL,
                         PRIMARY KEY (vname),
                         UNIQUE (building, street, city, state, zip)
                     );
 CREATE TABLE concert    (   cid INT AUTO_INCREMENT,
-                            cname VARCHAR(20) NULL,
-                            vname VARCHAR(20) NOT NULL,
+                            cname VARCHAR(50) NULL,
+                            vname VARCHAR(50) NOT NULL,
                             ctime CHAR(16) NOT NULL,  /*16 = sizeof('yyyy-mm-dd hh:mm')*/
                             tkturl varchar(64) NULL,
                             cover DECIMAL NOT NULL,
@@ -54,32 +54,32 @@ CREATE TABLE concert    (   cid INT AUTO_INCREMENT,
                             UNIQUE (vname, ctime),
                             FOREIGN KEY (vname) REFERENCES venue(vname)
                         );
-CREATE TABLE genre    (     gname VARCHAR(20),
-                            gparent VARCHAR(20) NULL,
+CREATE TABLE genre    (     gname VARCHAR(50),
+                            gparent VARCHAR(50) NULL,
                             PRIMARY KEY (gname),
                             FOREIGN KEY (gparent) REFERENCES genre(gname)
                         );
-CREATE TABLE band_links (   bname VARCHAR(20) NOT NULL,
-                            linktext VARCHAR(20) NULL,
+CREATE TABLE band_links (   bname VARCHAR(50) NOT NULL,
+                            linktext VARCHAR(50) NULL,
                             FOREIGN KEY (bname) REFERENCES band(bname)
                         );
-CREATE TABLE rel_user_fan_band  (   uname VARCHAR(20) NOT NULL,
-                                    bname VARCHAR(20) NOT NULL,
+CREATE TABLE rel_user_fan_band  (   uname VARCHAR(50) NOT NULL,
+                                    bname VARCHAR(50) NOT NULL,
                                     fdate CHAR(10) /*10 = sizeof('yyyy-mm-dd')*/,
                                     FOREIGN KEY (uname) REFERENCES user(uname),
                                     FOREIGN KEY (bname) REFERENCES band(bname)
                                 );
-CREATE TABLE rel_user_likes_genre   (   uname VARCHAR(20) NOT NULL,
-                                        gname VARCHAR(20) NOT NULL,
+CREATE TABLE rel_user_likes_genre   (   uname VARCHAR(50) NOT NULL,
+                                        gname VARCHAR(50) NOT NULL,
                                         FOREIGN KEY (uname) REFERENCES user(uname),
                                         FOREIGN KEY (gname) REFERENCES genre(gname)
                                 );
-CREATE TABLE rel_user_follows_user  (   follower VARCHAR(20) NOT NULL,
-                                        followee VARCHAR(20) NOT NULL,
+CREATE TABLE rel_user_follows_user  (   follower VARCHAR(50) NOT NULL,
+                                        followee VARCHAR(50) NOT NULL,
                                         FOREIGN KEY (follower) REFERENCES user(uname),
                                         FOREIGN KEY (followee) REFERENCES user(uname)
                                     );
-CREATE TABLE rel_user_attends_concert   (   uname VARCHAR(20) NOT NULL,
+CREATE TABLE rel_user_attends_concert   (   uname VARCHAR(50) NOT NULL,
                                             cid INT NOT NULL,
                                             review VARCHAR(64) NULL,
                                             rating float NULL,
@@ -87,31 +87,31 @@ CREATE TABLE rel_user_attends_concert   (   uname VARCHAR(20) NOT NULL,
                                             FOREIGN KEY (uname) REFERENCES user(uname),
                                             FOREIGN KEY (cid) REFERENCES concert(cid)
                                         );
-CREATE TABLE rel_band_plays_genre   (   bname VARCHAR(20) NOT NULL,
-                                        gname VARCHAR(20) NOT NULL,
+CREATE TABLE rel_band_plays_genre   (   bname VARCHAR(50) NOT NULL,
+                                        gname VARCHAR(50) NOT NULL,
                                         FOREIGN KEY (bname) REFERENCES band(bname),
                                         FOREIGN KEY (gname) REFERENCES genre(gname)
                                     );
-CREATE TABLE rel_band_performs_concert  (   bname VARCHAR(20) NOT NULL,
+CREATE TABLE rel_band_performs_concert  (   bname VARCHAR(50) NOT NULL,
                                             cid INT NOT NULL,
                                             FOREIGN KEY (bname) REFERENCES band(bname),
                                             FOREIGN KEY (cid) REFERENCES concert(cid)
                                         );
-CREATE TABLE recolist   (   lname VARCHAR(20) NOT NULL,
-                            uname VARCHAR(20) NOT NULL,
-                            gname VARCHAR(20) NOT NULL,
+CREATE TABLE recolist   (   lname VARCHAR(50) NOT NULL,
+                            uname VARCHAR(50) NOT NULL,
+                            gname VARCHAR(50) NOT NULL,
                             PRIMARY KEY (lname),
                             FOREIGN KEY (uname) REFERENCES user(uname),
                             FOREIGN KEY (gname) REFERENCES genre(gname)
                         );
-CREATE TABLE rel_recolist_contains_concert  (   lname INT NOT NULL,
+CREATE TABLE rel_recolist_contains_concert  (   lname VARCHAR(50) NOT NULL,
                                                 cid INT NOT NULL,
                                                 FOREIGN KEY (lname) REFERENCES recolist(lname),
                                                 FOREIGN KEY (cid) REFERENCES concert(cid)
                                             );
-CREATE TABLE user_input (   uname VARCHAR(20) NOT NULL,
+CREATE TABLE user_input (   uname VARCHAR(50) NOT NULL,
                             cid INT NOT NULL,
-                            vname VARCHAR(20) NULL,
+                            vname VARCHAR(50) NULL,
                             ctime CHAR(16) NULL,
                             tkturl varchar(64) NULL,
                             cover DECIMAL NULL,
