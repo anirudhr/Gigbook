7b6f27962887ef226a2bd71059e2601412b9c2bf
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-16 19:20:50 -0500

    Suggestion SPs. Need to add Band Popularity in schema, possibly as a view (need to change user reputation too, btw).

---
+++ SPs/select_band_suggest_list.sql
@@ -0,0 +1,9 @@
+/*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
+/*SP to select a list of bands sorted in descending order by:
+-> band popularity . does user like genre*/
+DELIMITER //
+CREATE PROCEDURE sp_band_suggest_list()
+BEGIN
+    
+END//
+DELIMITER ;
