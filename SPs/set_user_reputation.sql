2d6d50b49333746aff401802e2ba01dd330a9acf
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 11:20:17 -0500

    should be using triggers for reputation and popularity, aaaaargh

--- SPs/set_user_reputation.sql
+++ SPs/set_user_reputation.sql
@@ -1,8 +1,3 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
-/*SP to  create a list of recommended concerts as a user and update the user's reputation.*/
-DELIMITER //
-CREATE PROCEDURE sp_user_reputation()
-BEGIN
-    INSERT INTO recolist (lname, uname, gname) VALUES (inp_lname, inp_uname, inp_gname);
-END//
-DELIMITER ;
+/*Trigger to  set the user's reputation.*/
+
