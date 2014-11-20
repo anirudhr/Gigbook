1a4521b69f6aa423e2f099c4d25706556fe2480b
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-19 22:06:59 -0500

    restarting SPs on popularity and reputation

--- SPs/set_band_popularity.sql
+++ SPs/set_band_popularity.sql
@@ -1,3 +1,8 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Trigger to set the band's popularity with an SP.*/
-
+DELIMITER //
+CREATE PROCEDURE sp_band_popularity()
+BEGIN
+    
+END//
+DELIMITER ;
--- SPs/set_user_reputation.sql
+++ SPs/set_user_reputation.sql
@@ -1,3 +1,8 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Trigger to set the user's reputation with an SP.*/
-
+DELIMITER //
+CREATE PROCEDURE sp_user_reputation()
+BEGIN
+    
+END//
+DELIMITER ;
