168195cba1cd9b555c689d9bf60f45142afd2d96
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-20 11:22:24 -0500

    plan updated

--- SPs/get_band_popularity.sql
+++ SPs/get_band_popularity.sql
@@ -3,6 +3,6 @@
 DELIMITER //
 CREATE PROCEDURE sp_band_popularity(IN bandname VARCHAR(50), OUT popularity FLOAT DEFAULT 0)
 BEGIN
-    
+    DECLARE popA, popB, popC, popD, popE FLOAT DEFAULT 0;
 END//
 DELIMITER ;
