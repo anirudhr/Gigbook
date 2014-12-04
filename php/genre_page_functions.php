559615ede5874e5c1d88af9f6d50cfa2ca01ad62
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-04 11:22:27 -0500

    working on genre page

---
+++ php/genre_page_functions.php
@@ -0,0 +1,42 @@
+<?php
+function get_bands_playing_genre($mysqli, $gname) {// Returns array of band names playing a specific genre sorted by popularity
+  $stmt = $mysqli->stmt_init();
+  $get_bands_playing_genre_query = "SELECT rel_band_plays_genre.bname FROM rel_band_plays_genre WHERE gname = ?";
+  if(!$stmt->prepare($get_bands_playing_genre_query)) {
+    throw new Exception("get_bands_playing_genre: failed to prepare");
+  }
+  else {
+    $stmt->bind_param('s', $gname);
+		if (!$stmt->execute()) {
+			throw new Exception("get_bands_playing_genre: failed to execute");
+		}
+		$bname = NULL;
+		$stmt->bind_result($cid, $cname, $bname);
+		$bnames = array();
+		while($stmt->fetch()) {
+			array_push($bnames, $bname);
+		}
+		$stmt = $mysqli->stmt_init();
+		$get_popularity_by_band_query = "CALL sp_band_popularity(?, @popularity)";
+		if(!$stmt->prepare($get_popularity_by_band_query)) {
+      throw new Exception("get_popularity_by_band_query: failed to prepare");
+    }
+    else {
+      $bandbypopularities = array();
+      foreach ($bnames as $bandname) {
+        $stmt->bind_param('s', $bandname);
+        if (!$stmt->execute()) {
+          throw new Exception("get_popularity_by_band_query: failed to execute");
+        }
+        $stmt->fetch();
+        $result = $mysqli->query('SELECT @popularity as popularity');
+        $row = $result->fetch_assoc();
+        $bandbypopularities[$row['popularity']] = $bandname;
+      }
+      krsort($bandbypopularities);//sort high to low by keys
+      return $bandbypopularities;
+		}
+	}
+}
+?>
+<!--/*:indentSize=2:tabSize=2:noTabs=true:wrap=soft:*/-->
