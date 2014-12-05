0e458635629d5162a53e5679467ce14a0e84485f
Sherlyn Menezes <sherlyn.menezes@gmail.com>
2014-12-04 23:26:19 -0500

    my php files

--- php/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "gigbook_schema");
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-?>
