af78938cb4e092189da1283681ff20466cacde22
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-11 12:56:13 -0500

    Concert addition now works

--- SherlPHP/postConcerts.php
+++ SherlPHP/postConcerts.php
@@ -1,35 +1,29 @@
 <?php
+//:indentSize=4:tabSize=4:noTabs=true:wrap=soft:
 include ("connectdb.php");
 $bname=$_POST['bname'];
 $cname=$_POST['cname'];
 $vid=$_POST['vid'];
-$ctime=$_POST['cdate'].$_POST['ctime'];
+$ctime=$_POST['cdate'] . " " . $_POST['ctime'];
 $tkturl=$_POST['tkturl'];
 $cover=$_POST['cover'];
 $avail=$_POST['avail'];
+//print "***" . $bname . ":" . $cname . ":" . $vid . ":" . $ctime . ":" . $tkturl . ":" . $cover . ":" . $avail . "<br/>";
 
-if($stmt = $mysqli->prepare("INSERT INTO concert (cname,vid,ctime,postedtime,tkturl,cover,availability)VALUES (?,?,?,now(),?,?,?)")) {
-	
-	$stmt->bind_param('sissss', $cname,$vid,$ctime,$tkturl,$cover,$avail);
-	
-  $stmt->execute();
- 
-
+if(!$stmt = $mysqli->prepare("INSERT INTO concert (cname,vid,ctime,postedtime,tkturl,cover,availability)VALUES (?,?,?,now(),?,?,?)")) {
+	throw new Exception("New concert insertion: prepare failed: " . $mysqli->error);
 }
-if($stmt2 = $mysqli->prepare("select cid from concert where cname = ? )")) {
-	
-	$stmt2->bind_param('s', $cname);
-	
-  $stmt2->execute();
-  $stmt2->bind_result('i',$cid);
-  echo $cid;
-
+$stmt->bind_param('sissss', $cname,$vid,$ctime,$tkturl,$cover,$avail);
+if (!$stmt->execute()) {
+    throw new Exception("New concert insertion: execute failed: " . $mysqli->error);
 }
-
- 
-
-
-else
-echo "Unsuccesful";
-	
-	?>
+if(!$stmt3 = $mysqli->prepare("INSERT INTO rel_band_performs_concert (cid,bname)
+    SELECT cid, ? FROM concert WHERE cname = ? AND ctime = ?")) {
+	throw new Exception("New concert band performance insertion: prepare failed: " . $mysqli->error);
+}
+$stmt3->bind_param('sss', $bname, $cname, $ctime);
+if (!$stmt3->execute()) {
+    throw new Exception("New concert band performance insertion: execute failed: " . $mysqli->error);
+}
+echo "<script language='javascript'>javascript:history.go(-1)</script>";
+?>
