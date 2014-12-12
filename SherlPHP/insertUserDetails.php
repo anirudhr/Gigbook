8c20dfbba622e882ac11f47e87e7d71e628737ae
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-12 00:41:11 -0500

    still trying

--- SherlPHP/insertUserDetails.php
+++ SherlPHP/insertUserDetails.php
@@ -8,6 +8,20 @@
 <body>
 <?php
 include "connectdb.php";
+
+function getext($imagetype) {
+    if(empty($imagetype)) {
+        return false;
+    }
+    switch($imagetype) {
+        case 'image/bmp': return '.bmp';
+        case 'image/gif': return '.gif';
+        case 'image/jpeg': return '.jpg';
+        case 'image/png': return '.png';
+        default: return false;
+    }
+}
+
 $uname=$_POST["uname"];
 $pwd=$_POST["pass"];
 $firstname = $_POST["fname"];
@@ -15,38 +29,34 @@ $lastname = $_POST["lname"];
 $city=$_POST["city"];
 $email=$_POST["uemail"];
 $dob=$_POST["DOB"];
-
-function GetImageExtension($imagetype) {
-    if(empty($imagetype)) {
-        return false;
+$target_dir = "images/user/";
+$target_file = $target_dir . $uname . ".jpg";
+$uploadOk = 1;
+$imageFileType = getext($_FILES["uploadedimage"]["type"]);
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["uploadedimage"]["tmp_name"]);
+    if($check == false) {
+        throw new Exception("File is not an image.");
+        $uploadOk = 0;
     }
-    switch($imagetype) {
-       case 'image/bmp': return '.bmp';
-       case 'image/gif': return '.gif';
-       case 'image/jpeg': return '.jpg';
-       case 'image/png': return '.png';
-       default: return false;
+    if (file_exists($target_file)) {
+        throw new Exception("Sorry, file already exists.");
+        $uploadOk = 0;
     }
-}
- 
- 
- 
-if (!empty($_FILES["uploadedimage"]["name"])) {
-    $file_name=$_FILES["uploadedimage"]["name"];
-    $temp_name=$_FILES["uploadedimage"]["tmp_name"];
-    $imgtype=$_FILES["uploadedimage"]["type"];
-    $ext= GetImageExtension($imgtype);
-    $imagename=$uname.$ext;
-    $target_path = "images/user/".$imagename;
-    if(!move_uploaded_file($temp_name, $target_path)) {
-        throw new Exception("Image upload failed");
+    if($imageFileType != ".jpg") {
+        throw new Exception("Sorry, only JPG files are allowed. Uploaded file is " . $imageFileType);
+        $uploadOk = 0;
     }
-}
-else {
-    $temp_name  = "images/user/blank.jpg";
-    $target_path = "images/user/".$uname.".jpg";
-    if(!copy($temp_name, $target_path)) {
-        throw new Exception("Blank image copy failed");
+    if ($uploadOk) {
+            if (move_uploaded_file($_FILES["uploadedimage"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["uploadedimage"]["name"]). " has been uploaded.";
+        }
+        else {
+            throw new Exception("Sorry, there was an error uploading your file.");
+        }
+    }
+    else {
+        throw new Exception("Upload failed.");
     }
 }
 $stmt = $mysqli->stmt_init();
